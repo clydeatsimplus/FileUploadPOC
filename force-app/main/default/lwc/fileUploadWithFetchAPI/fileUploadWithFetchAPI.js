d9b88f70a7b089573a35b62d94f1b44b54b38714
clydeatsimplus <clydeenrique.tan@simplus.com>
2023-09-05 18:17:21 +0800

    WIP

--- force-app/main/default/lwc/fileUploadWithFetchAPI/fileUploadWithFetchAPI.js
+++ force-app/main/default/lwc/fileUploadWithFetchAPI/fileUploadWithFetchAPI.js
@@ -5,19 +5,40 @@ import { loadScript } from 'lightning/platformResourceLoader';
 let XLS = {};
 
 export default class FileUploadWithFetchAPI extends LightningElement {
-    endpoint = 'https://fileuploadpoc-dev-ed.develop.my.salesforce.com/services/data/v57.0/sobjects/ContentVersion';
+    endpoint = 'https://fileuploadpoc-dev-ed.develop.my.salesforce.com/services/data/v58.0/sobjects/ContentVersion';
     accessToken = '00D5j00000CuOHk!AR8AQKplGYQbqsJOKXwXHTGS9ctwkIfmsS4PBQbIkQumi9r3.Mjn7.03AR4JMxrZTGM1BWDDMqLNbK1H3FMlaJUFrp1pQm2W';
     body;
     @track acceptedFormats = ['.xls', '.xlsx'];
 
+
+    // insertContentVersion(){
+    //     fetch(this.endpoint, 
+    //         {
+    //             method:'POST', 
+    //             headers:{ 
+    //                 "Authorization": 'Bearer '+this.accessToken, 
+    //                 "Content-Type": "application/json"}, 
+    //             body: this.body
+    //         })
+    //     .then(response => response.text())
+    //     .then(text  => console.log(text))
+    //     .catch(error => console.log('error', error))
+    // }
+
+
     insertContentVersion(){
+        // for (var pair of this.body.entries()) {
+        //     console.log(pair[0]+ ', ' + pair[1]); 
+        // }
+        console.log('this.body', this.body);
         fetch(this.endpoint, 
             {
                 method:'POST', 
                 headers:{ 
                     "Authorization": 'Bearer '+this.accessToken, 
-                    "Content-Type": "application/json"}, 
-                body: this.body
+                    "Content-Type": "multipart/form-data; boundary=\"boundary_string\""}, 
+                    mode: "no-cors",
+                body: new FormData()
             })
         .then(response => response.text())
         .then(text  => console.log(text))
@@ -40,18 +61,83 @@ export default class FileUploadWithFetchAPI extends LightningElement {
         }
     }
 
+    // async readData(file){
+    //     var reader = new FileReader();
+    //     reader.onload = event => {
+    //         var base64 = reader.result.split(',');
+    //         var bodyObject = {};
+    //         bodyObject.Title = 'Insert From LWC 1';
+    //         bodyObject.VersionData = window.btoa(base64);
+    //         bodyObject.PathOnClient = 'InsertedFromLWC1.xlsx';
+    //         bodyObject.ContentLocation = 'S';
+    //         this.body = JSON.stringify(bodyObject);
+            
+    //     };
+    //     reader.onerror = function(ex) {
+    //         this.error=ex;
+    //         this.dispatchEvent(
+    //             new ShowToastEvent({
+    //                 title: 'Error while reding the file',
+    //                 message: ex.message,
+    //                 variant: 'error',
+    //             }),
+    //         );
+    //     };
+    //     reader.readAsBinaryString(file);
+    // }
+
+    /**
+     * @description form data test
+     * @param {*} file 
+     */
     async readData(file){
         var reader = new FileReader();
         reader.onload = event => {
+            console.log(window.btoa(reader.result));
             var base64 = reader.result.split(',');
-            var bodyObject = {};
-            bodyObject.Title = 'Insert From LWC 1';
-            bodyObject.VersionData = window.btoa(base64);
-            bodyObject.PathOnClient = 'InsertedFromLWC1.xlsx';
-            bodyObject.ContentLocation = 'S';
-            this.body = JSON.stringify(bodyObject);
-            
+            let data = "";
+            data += '--boundary_string\r\n';
+            data += 'Content-Type:application/octet-stream\r\n';
+            data += 'Content-Disposition: form-data; name="VersionData";'+ 'filename="SampleData(1011).xlsx"\r\n';
+            data += '\r\n';
+            data +=  reader.result + '\r\n';
+            data += '--boundary_string\r\n';
+            data += 'Content-Disposition: form-data; ' + 'name="entity_content";\r\n'
+            data += 'Content-Type: application/json\r\n';
+            data += '\r\n';
+            data += '{\r\n';
+            data += '"Title" : "Test title",\r\n';
+            data += '"ContentLocation" : "S", \r\n';
+            data += '"PathOnClient" : "SampleData(1011).xlsx"\r\n';
+            data += '}\r\n';                
+            data += '--boundary_string';
+            console.log('data-->'+data);
+
+            // const contentVersion = {
+            //     FirstPublishLocationId: "0055j000009SFYWAA4",
+            //     Title: 'Test File',
+            //     PathOnClient: 'test.xlsx',
+            //     Origin: "C",
+            // };
+
+            // var formDataObject = new FormData();
+            // formDataObject.setBoundary('boundary_string');
+            // formDataObject.append("entity_content", JSON.stringify(contentVersion), {
+            //     contentType: "application/json",
+            // });
+
+            // formDataObject.append("VersionData", window.btoa(base64), {
+            //     filename: 'test.xlsx',
+            //     contentType: 'EXCEL_X',
+            // });
+
+            // formDataObject.append('Title', 'Test File');
+            // formDataObject.append('VersionData', window.btoa(base64));
+            // formDataObject.append('PathOnClient', 'test.xlsx');
+            // formDataObject.append('ContentLocation', 'S');
+            this.body = data;
         };
+
         reader.onerror = function(ex) {
             this.error=ex;
             this.dispatchEvent(
@@ -66,6 +152,7 @@ export default class FileUploadWithFetchAPI extends LightningElement {
     }
 
     handleClick(){
+        console.log('handleClick');
         this.insertContentVersion();
     }
 
@@ -88,31 +175,22 @@ export default class FileUploadWithFetchAPI extends LightningElement {
       }
 
     postFormData(data) {
-        const formData = new FormData();
-        formData.append('fileData', JSON.stringify(data));
+        let formData = new FormData();
+        //formData.append('fileData', JSON.stringify(data));
         formData.append('PathOnClient', 'test.xlsx');
         formData.append('Title', 'Test File');
-        formData.append('VersionData', data);
+        formData.append('VersionData', window.btoa(data));
         console.log('formData', formData);
         fetch(this.endpoint, {
           method: 'POST',
-          body: formData,
           headers: {
             'Authorization': 'Bearer ' + this.accessToken,
             'Content-Type': 'multipart/form-data'
-          }
+          },
+          body: formData
         })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          console.log(data);
-        })
-        .catch(error => {
-          console.error('There was a problem with the fetch operation:', error);
-        });
+        .then(response => response.text())
+        .then(text  => console.log(text))
+        .catch(error => console.log('error', error))
       }
 }
